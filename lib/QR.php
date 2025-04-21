fdf54b9cd8efe37914b646bf1ec2b824eb50105b
KamikaLeigh <kamikaleigh@gmail.com>
2025-04-21 08:14:36 +0100

    PHP for QR table (Not uploaded to server)

---
+++ lib/QR.php
@@ -0,0 +1,48 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Headers: *");
+header("Content-Type: application/json");
+
+///
+///
+///
+
+$conn = new mysqli('localhost', '2350900', 'Lavinder2010', 'db2350900');
+
+if ($conn -> connect_error){
+    echo json_encode(['error' => 'Database could not be accessed']);
+    exit;
+}
+
+
+function NumScan($conn, $CourseCode) {
+    try{
+    $statement = "UPDATE QR SET NumScan = NumScan + 1 WHERE CourseCode = $CourseCode";
+
+    if ($conn->query($statement) === TRUE) {
+        echo "Record updated successfully";
+    } else {
+        echo "Error updating record: ". $conn->error;
+    }
+    } catch (Exception $e) {
+        echo "Exception occurred: ". $e->getMessage(); //Error Handling
+    }
+    $conn->close();// Close connection
+
+}
+
+function Interested($conn, $CourseCode) {
+    try{
+    $statement = "UPDATE QR SET Interested = Interested + 1 WHERE CourseCode = $CourseCode";
+
+    if ($conn->query($statement) === TRUE) {
+        echo "Record updated successfully";
+    } else {
+        echo "Error updating record: ". $conn->error;
+    }
+    } catch (Exception $e) {
+        echo "Exception occurred: ". $e->getMessage(); //Error Handling
+    }
+    $conn->close();// Close connection
+}
+?>
