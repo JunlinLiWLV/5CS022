714e8472de057f7e33bded0a3de01e2e3d6628b0
rdot6879 <r.okell@wlv.ac.uk>
2025-05-05 20:37:40 +0100

    Can now fetch users from external server

---
+++ lib/server/usermanagement.php
@@ -0,0 +1,55 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Headers: *");
+header("Content-Type: application/json");
+
+///
+///
+///
+
+$conn = new mysqli('localhost', '2336879', 'v1zh41', 'db2336879');
+
+if ($conn -> connect_error){
+    echo json_encode(['error' => 'Database could not be accessed']);
+    exit;
+}
+
+
+function GetAll(){
+    global $conn;
+
+    $statement = "SELECT ID, Name, Email, Phone, Contact FROM users"; //Query
+    $result = $conn -> query($statement); //Execute Query
+
+    $rows = array();
+    while ($row = $result -> fetch_assoc()){
+        $rows[] = $row;
+    }
+
+    print json_encode($rows);
+
+    $result -> free_result();
+    $conn -> close(); //Close connection
+
+}
+
+
+//function addUser($conn, $name, $email, $phone, $contact) {
+//    try{
+//        $statement = $conn->prepare("INSERT INTO Guests (Name, Email, telephone, Contact) VALUES (?, ?, ?, ?)"); //Query
+//        $statement->bind_param("sssi", $name, $email, $phone, $contact); //Use entered Data
+//        //Execute query
+//        if ($statement->execute()) {
+//            echo "New record created successfully";
+//        } else {
+//            echo "Error: ". $statement->error; //Error Handling
+//        }
+//        $statement -> close(); //Close Query
+//    } catch (Exception $e) {
+//        echo "Exception occurred: ". $e->getMessage(); //Error Handling
+//    }
+//    $conn->close(); //Close connection
+//}
+
+
+?>
