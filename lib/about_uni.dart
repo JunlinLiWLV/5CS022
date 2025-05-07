66bbc96868c58f3053a6cce4179fc67ceb72e255
rdot6879 <r.okell@wlv.ac.uk>
2025-05-07 21:27:20 +0100

    Overhauled page, now displays the University's about page

--- lib/about_uni.dart
+++ lib/about_uni.dart
@@ -1,38 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 import 'course_info.dart';
 
 class aboutUni extends StatelessWidget{
-  
+
+  late final WebViewController _controller = WebViewController()
+  ..setJavaScriptMode(JavaScriptMode.unrestricted)
+  ..loadRequest(Uri.parse('https://www.wlv.ac.uk/about-us/'));
+
+  // @override
+  // void initState(){
+  //   _controller = WebViewController();
+  //   _controller.setJavaScriptMode(JavaScriptMode.unrestricted);
+  //   _controller.loadRequest(Uri.parse('https://www.wlv.ac.uk/about-us/'));
+  // }
+
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context){
     return Scaffold(
-      appBar: AppBar(title: Text("About our University"),),
-      body: Padding(
-        padding: EdgeInsets.all(15.0),
-        child: Column(
-          children: [
-            Row(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                Flexible(child: Text("From its origins in the 1820s, the University of Wolverhampton has evolved to offer a wide range of courses, from Art "
-                    "to Pharmaceutical Science, with study options ranging from foundation to postgraduate degrees."),
-                ),
-              ],
-            ),
-            Row(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                SizedBox( height: 10, ),
-                Flexible(child: Text("At the University of Wolverhampton, we are proud to describe ourselves as the University of Opportunity, demonstrating our commitment to "
-                    "making higher education more accessible and welcoming for prospective students, no matter their socioeconomic backgrounds. With over 29,000 students worldwide, and 2,100 staff, we strive to"
-                    " deliver the best experiences possible, helping our students to thrive and fulfil their potential as they move into the next exciting chapter of their lives"))
-              ],
-            )
-          ],
+        appBar: AppBar(
+          leading: InkWell(
+            onTap: () async {
+              if (await _controller.canGoBack()) {
+                await _controller.goBack();
+              } else {
+                Navigator.pop(context);
+              }
+            },
+            child: const Icon(Icons.arrow_back),
+          ),
+          title: Text("About Us", style: TextStyle(fontSize: 24, fontWeight: FontWeight.w600, fontFamily: 'RobotoSlab'),),
         ),
-      )
+        body: WebViewWidget(controller: _controller,)
     );
   }
+
+
 }
 
 class theCourses extends StatelessWidget{
